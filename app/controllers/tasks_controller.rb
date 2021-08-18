e546295c27e73249776e859562f99a8e73dd10d5
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-08-18 11:15:57 +0900

    flashメッセージ追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:notice]="投稿を作成しました！"
+      flash[:notice]="タスクを作成しました！"
       redirect_to tasks_path
     else
       render :new
@@ -27,7 +28,20 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path, notice:"ブログを削除しました！"
+    redirect_to tasks_path, notice:"タスクを削除しました！"
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
+  end 
+
+  def confirm
+    @task = Task.new(task_params)
   end
 
   private
