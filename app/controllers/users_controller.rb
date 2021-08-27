090a3f49ba224e4464ee36f2e432a6e1b4d3dcb4
Nakaji <85724781+Nakajima-Yuuki@users.noreply.github.com>
2021-08-27 16:29:48 +0900

    アソシエーション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,35 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-      end
-      def create
-        @user = User.new(user_params)
-        if @user.save
-            # 保存の成功した場合の処理
-          else
-            render :new
-        end
-      end
-
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to user_path(@user.id)
-        else
-          render :new
-        end
-      end
-      private
-      def user_params
-        params.require(:user).permit(:full_name, :email, :password,
-                                     :password_confirmation)
-      end
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+    @user = User.new
+  end
+      
+  def create
+    @user = User.new(user_params)
+     if @user.save
+    # 保存の成功した場合の処理
+     else
+    render :new
     end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+       render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @task = @user.tasks
+  end
+ 
+  private
+    def user_params
+     params.require(:user).permit(:full_name, :email, :password,
+                                     :password_confirmation)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_151059) do
+ActiveRecord::Schema.define(version: 2021_08_27_072629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_08_26_151059) do
     t.datetime "expiration_date"
     t.string "status"
     t.integer "priority", default: 0
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_08_26_151059) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
